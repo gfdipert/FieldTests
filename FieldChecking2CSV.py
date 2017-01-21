5805cec88bc7e97c09e743e5e0e427a443dbb072
Gwendolyn Dipert <gwendipert@ziftsolutions.com>
2017-01-21 20:13:05 +0000

    FieldChecking2CSV.py edited online with Bitbucket

--- FieldChecking2CSV.py
+++ FieldChecking2CSV.py
@@ -1,24 +1,8 @@
 import os
-import glob
-
-csvs = []
-#csvnames = []
-
-for root,dirs,files in os.walk("./"):
-	for file in files:
-		if file.endswith((".csv")):
-			csvs.append(file)
-
 
 from FieldTests2CSV import FieldTests
 refcsvinput = raw_input('Enter reference CSV file name: ')
 csvinput = raw_input('Enter CSV file you would like to check: ')
-
-for file in csvs:
-	if refcsvinput == file:
-		REFCSV = file
-	if csvinput == file:
-		CSV = file
 
 with open(CSV) as csvfile:
     with open(REFCSV) as refcsvfile:
