ce9b4a4d82aaa472888db067c37ce166ed1e92f9
Gwen Dipert <gwendipert@Zift-Gwen-Dipert-MacBook-Pro.local>
2017-01-21 15:16:25 -0500

    Accessing directories first commit

--- FieldChecking2CSV.py
+++ FieldChecking2CSV.py
@@ -1,8 +1,23 @@
 import os
-
+import glob
 from FieldTests2CSV import FieldTests
+
+csvs = []
+#csvnames = []
+
+for root,dirs,files in os.walk("./"):
+	for file in files:
+		if file.endswith((".csv")):
+			csvs.append(file)
+
 refcsvinput = raw_input('Enter reference CSV file name: ')
 csvinput = raw_input('Enter CSV file you would like to check: ')
+
+for file in csvs:
+	if refcsvinput == file:
+		REFCSV = file
+	if csvinput == file:
+		CSV = file
 
 with open(CSV) as csvfile:
     with open(REFCSV) as refcsvfile:
